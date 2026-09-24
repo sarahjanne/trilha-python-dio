a481352a7faf0104be7029d355eebb8109d31cd1
Sarah Janne <sarahjanne.enos@hotmail.com>
2026-09-24 10:11:42 -0300

    Add pyproject.toml for database project and VSCode settings for poetry integration
    - Created pyproject.toml for the "07-banco-de-dados" project with dependencies on FastAPI and psycopg.
    - Added VSCode settings to configure the default environment and package manager to use poetry for the "dioBank" project.

--- 01 - Estrutura de dados/04 - Dicionários/00_declarando_dicionarios.py
+++ 01 - Estrutura de dados/04 - Dicionários/00_declarando_dicionarios.py
@@ -6,3 +6,6 @@ print(pessoa)
 
 pessoa["telefone"] = "3333-1234"  # {"nome": "Guilherme", "idade": 28, "telefone": "3333-1234"}
 print(pessoa)
+
+pessoa["nome"] = "Sarah"
+print(pessoa)  # {"nome": "Sarah", "idade": 28, "telefone": "3333-1234"}
